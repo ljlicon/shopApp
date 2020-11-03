9dc4190609559164a52a6e7a4dbabecf59d283f4
wylly <wyl@qq.com>
2020-11-03 17:55:27 +0800

    cart修改

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,3 +1,4 @@
+
 Page({
   /**
    * 页面的初始数据
@@ -12,17 +13,42 @@ Page({
   onLoad: function (options) {
     console.log(options)
   },
-  
+  /**
+   * 获取收获地址wxwx.chooseAddress
+   * 
+   * 
+   */
   handleGetAdress(){
-    wx.chooseAddress({
-      success: (result) => {
-        console.log(result)
-      },
-    })
+    
+    // wx.openSetting()
+    // wx.chooseAddress({
+    //   success: (result) => {
+    //     console.log(result)
+    //   },
+    // })
     wx.getSetting({
-      success:res=> {
-        console.log(res);
-        
+      success:(res)=> {
+        console.log(res.authSetting["scope.address"]);
+        if(res.authSetting["scope.address"]){
+          //如果他的权限是true那么就调用地址接口
+          wx.chooseAddress({
+              success: (result) => {
+                console.log(result)
+              },
+            })
+        }else{
+          console.log('那么就去打开权限')
+          // 其实现在在手机上他一直是true 这部已经没有必要了
+          wx.openSetting({
+            success (res) {
+              console.log(res.authSetting)
+              // res.authSetting = {
+              //   "scope.userInfo": true,
+              //   "scope.userLocation": true
+              // }
+            }
+          })
+        }
       }
     })
   },

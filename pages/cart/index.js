584333a1f4ece54bd9d57a7a02f32637111452f5
wylly <wyl@qq.com>
2020-11-04 17:15:32 +0800

    cart地址改好了

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,55 +1,65 @@
-
+import {
+  getSetting,
+  chooseAddress,
+  openSetting
+} from "../../utils/asyncWechat.js";
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    address:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+   
   },
   /**
-   * 获取收获地址wxwx.chooseAddress
-   * 
-   * 
+   * 生命周期函数--监听页面显示
    */
-  handleGetAdress(){
-    
-    // wx.openSetting()
-    // wx.chooseAddress({
-    //   success: (result) => {
-    //     console.log(result)
-    //   },
-    // })
-    wx.getSetting({
-      success:(res)=> {
-        console.log(res.authSetting["scope.address"]);
-        if(res.authSetting["scope.address"]){
-          //如果他的权限是true那么就调用地址接口
-          wx.chooseAddress({
-              success: (result) => {
-                console.log(result)
-              },
-            })
-        }else{
-          console.log('那么就去打开权限')
-          // 其实现在在手机上他一直是true 这部已经没有必要了
-          wx.openSetting({
-            success (res) {
-              console.log(res.authSetting)
-              // res.authSetting = {
-              //   "scope.userInfo": true,
-              //   "scope.userLocation": true
-              // }
-            }
-          })
-        }
-      }
+  onShow: function () {
+    const address = wx.getStorageSync('address');
+    this.setData({
+      address
     })
+  },
+  async handleGetAdress() {
+    //获取权限状态  看下是不是true 是的话就调用地址的方法不是就打开设置
+    const res = await getSetting();
+    const scopeAddress=res.authSetting["scope.address"]
+    if(!scopeAddress){
+     await openSetting()
+    }
+    const addressInfo =await chooseAddress()
+    wx.setStorageSync('address', addressInfo)
+    // wx.getSetting({
+    //   success: (res) => {
+    //     console.log(res.authSetting["scope.address"]);
+    //     if (res.authSetting["scope.address"]) {
+    //       //如果他的权限是true那么就调用地址接口
+    //       wx.chooseAddress({
+    //         success: (result) => {
+    //           console.log(result)
+    //         },
+    //       })
+    //     } else {
+    //       console.log('那么就去打开权限')
+    //       // 其实现在在手机上他一直是true 这部已经没有必要了
+    //       wx.openSetting({
+    //         success(res) {
+    //           console.log(res.authSetting)
+    //           // res.authSetting = {
+    //           //   "scope.userInfo": true,
+    //           //   "scope.userLocation": true
+    //           // }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
+
   },
 })
---
+++ utils/asyncWechat.js
@@ -0,0 +1,41 @@
+// Promise 形式的  获取用户的当前设置
+export const getSetting = () => {
+  return new Promise((resolve, reject) => {
+    wx.getSetting({
+      success: (result) => {
+        resolve(result)
+      },
+      fail: (err) => {
+        reject(err)
+      },
+    })
+  })
+}
+
+// Promise 形式的  获取用户的地址
+export const chooseAddress = () => {
+  return new Promise((resolve, reject) => {
+    wx.chooseAddress({
+      success: (result) => {
+        resolve(result)
+      },
+      fail: (err) => {
+        reject(err)
+      },
+    })
+  })
+}
+
+// Promise 形式的  调起客户端小程序设置界面
+export const openSetting = () => {
+  return new Promise((resolve, reject) => {
+    wx.openSetting({
+      success: (result) => {
+        resolve(result)
+      },
+      fail: (err) => {
+        reject(err)
+      },
+    })
+  })
+}
